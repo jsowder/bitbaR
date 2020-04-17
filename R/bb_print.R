13083f47590d553edec04b19c4dd7dbdbf8bfce1
Jacob Sowder <jacobsowder@gmail.com>
2020-04-17 14:40:53 -0400

    simplify bb_print

--- R/bb_print.R
+++ R/bb_print.R
@@ -1,41 +1,23 @@
-#' @title Print strings in BitBar fashion
+#' @title Print text to BitBar.
 #'
-#' @description I print, BitBar-style.
-#'
-#' @param ... What to print. Vectors are concatenated.
-#' @param nest Level of nesting Defaults to 0.
-#' @param sep What to seperate vectors by. Defaults to "".
-#' @param collapse What to collapse vectors by. Defaults to "".
+#' @param ... What to print. Concatenates arguments without spaces.
 #'
 #' @return Nothing
 #'
-#' @examples
-#' bb_print(nest = 1, c("this", "that"))
-#' bb_print(nest = 2, "this", "that")
-#' bb_print(nest = 3, c("this", "that"), "and the other")
-#' bb_print(nest = 4, c("2 + 2 = ", 2 + 2))
-#'
 #' @export
+#' @examples
+#' bb_print("text")
+#' bb_print(2 + 2)
+#' bb_print("text", "more text")
+#' bb_print("google", bb_attributes(href = "https://www.google.com""))
+#' bb_print("google | href=https://www.google.com")
 
 bb_print <- function(
   ...
-  , nest = 0
-  , sep = ""
-  , collapse = ""
 ){
-  null_results <-
-    list(...) %>%
-    Filter(f = length) %>% # Drops empties.
-    lapply(function(x){
-      cat(
-        strrep("--", nest)
-        , paste(
-          x
-          , sep = sep
-          , collapse = collapse
-        )
-        , sep = ""
-        , fill = TRUE
-      )
-    })
+  cat(
+    ...
+    , sep = ""
+    , fill = TRUE
+  )
 }
